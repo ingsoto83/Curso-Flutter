7c84a6cfeb544af2495515cfdef92cff6adf0f05
Ing. Alejandro Soto <soto@itsaltillo.edu.mx>
2020-01-17 10:59:12 -0600

    Código Viernes
    Firebase read & write

--- flutterfire_app/lib/home_page.dart
+++ flutterfire_app/lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_app/add_page.dart';
 
@@ -18,12 +19,40 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         //top: false,
         //bottom: false,
-        child: ListView(
-          children: <Widget>[
-            Center(
-              child:Text("Main Content")
-            )
-          ],
+        child: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('books').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError)
+              return new Text('Error: ${snapshot.error}');
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting: return new Text('Loading...');
+              default:
+                return new ListView(
+                  children: snapshot.data.documents.map((DocumentSnapshot document) {
+                    String title = document['title'];
+                    String year = document['year'];
+                    return new ListTile(
+                      onLongPress: (){
+                        Firestore.instance.collection("books").document(document.documentID).delete();
+                      },
+                      onTap: (){
+                        Firestore.instance.collection("books").document(document.documentID).updateData({
+                          'favorite' : !document['favorite']
+                        });
+                      },
+                      title: new Text("$title - $year"),
+                      subtitle: new Text(document['author']),
+                      leading: Container(
+                        //height: 100,
+                        //width: 100,
+                        child: Image.network(document['img'])
+                      ),
+                      trailing: document['favorite'] ? Icon(Icons.favorite, color: Colors.red,) : Icon(Icons.favorite_border)
+                    );
+                  }).toList(),
+                );
+            }
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
